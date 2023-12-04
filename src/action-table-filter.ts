240433347dc588dc1a7592078188e276d9349dd5
Colin Fahrion <colin.fahrion@gmail.com>
2023-12-03 17:11:38 -0800

    feat: Added switch component

--- src/action-table-filter.ts
+++ src/action-table-filter.ts
@@ -9,7 +9,7 @@ export class ActionTableFilterMenu extends HTMLElement {
 	// TODO: Add exact attribute that switches the filter to be exact match rather than includes
 
 	static get observedAttributes(): string[] {
-		return ["col", "options", "label", "switch"];
+		return ["col", "options"];
 	}
 
 	get col(): string {
@@ -18,14 +18,6 @@ export class ActionTableFilterMenu extends HTMLElement {
 
 	get options(): string {
 		return this.getAttribute("options") || "";
-	}
-
-	get label(): string {
-		return this.getAttribute("label") || "";
-	}
-
-	get switch(): string {
-		return this.getAttribute("switch") || "";
 	}
 
 	private addEventListeners(): void {
@@ -61,9 +53,9 @@ export class ActionTableFilterMenu extends HTMLElement {
 	}
 
 	private render(): void {
-		const html = `${this.label ? `<label part="label">${this.label}</label> ` : ""}<select part="select" data-col="${this.col}"><option value="">All</option>${this.options
-			.split(",")
-			.map((option) => `<option value="${option}">${option}</option>`)}</select>`;
+		const html = `<label part="label"><slot>Sort by</slot></label><select part="select" name="filter-${this.col}" data-col="${
+			this.col
+		}"><option value="">All</option>${this.options.split(",").map((option) => `<option value="${option}">${option}</option>`)}</select>`;
 		const css = `<style></style>`;
 
 		this.shadow.innerHTML = `${css}${html}`;
@@ -97,5 +89,89 @@ export class ActionTableFilterReset extends HTMLElement {
 	}
 }
 
+export class ActionTableFilterSwitch extends HTMLElement {
+	private shadow: ShadowRoot;
+
+	constructor() {
+		super();
+		this.shadow = this.attachShadow({ mode: "open" });
+	}
+
+	static get observedAttributes(): string[] {
+		return ["col", "filter"];
+	}
+
+	get col(): string {
+		return this.getAttribute("col")?.trim().toLowerCase() || "";
+	}
+
+	get filter(): string {
+		return this.getAttribute("filter")?.trim().toLowerCase() || "";
+	}
+
+	public connectedCallback(): void {
+		this.render();
+		this.addEventListeners();
+	}
+
+	private render(): void {
+		const css = `<style>
+        input {
+            appearance: none;
+            position: relative;
+            display: inline-block;
+            background: lightgrey;
+            height: 1.4em;
+            width: 2.75em;
+            vertical-align: middle;
+            border-radius: 2em;
+            box-shadow: 0px 1px 3px #0003 inset;
+            transition: 0.25s linear background;
+          }
+          input::before {
+            content: "";
+            display: block;
+            width: 1em;
+            height: 1em;
+            background: #fff;
+            border-radius: 1em;
+            position: absolute;
+            top: 0.2em;
+            left: 0.2em;
+            box-shadow: 0px 1px 3px #0003;
+            transition: 0.25s linear transform;
+            transform: translateX(0rem);
+          }
+          :checked {
+            background: green;
+          }
+          :checked::before {
+            transform: translateX(1rem);
+          }
+          input:focus {
+            outline: transparent;
+          }
+          input:focus-visible {
+            outline: 2px solid dodgerblue;
+            outline-offset: 2px;
+          }
+          </style>`;
+		const html = `<label>
+        <input type="checkbox" />
+        <slot>Show Only ${this.filter} for ${this.col}</slot>
+      </label>`;
+
+		this.shadow.innerHTML = `${css}${html}`;
+	}
+
+	private addEventListeners(): void {
+		// Add event listener that detects changes in the select element
+		this.shadow.addEventListener("click", () => {
+			this.dispatchEvent(new CustomEvent("action-table-filter-reset", { bubbles: true }));
+		});
+	}
+}
+
 customElements.define("action-table-filter-menu", ActionTableFilterMenu);
+customElements.define("action-table-filter-switch", ActionTableFilterSwitch);
 customElements.define("action-table-filter-reset", ActionTableFilterReset);
