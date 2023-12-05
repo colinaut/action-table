d667c3fef8433a9c30a42db857863401c23af254
Colin Fahrion <colin.fahrion@gmail.com>
2023-12-05 05:52:00 -0800

    feat: changed filter switch to light dom

--- src/action-table-filter-switch.ts
+++ src/action-table-filter-switch.ts
@@ -1,21 +1,22 @@
 export class ActionTableFilterSwitch extends HTMLElement {
-	private shadow: ShadowRoot;
-
 	constructor() {
 		super();
-		this.shadow = this.attachShadow({ mode: "open" });
 	}
 
 	static get observedAttributes(): string[] {
-		return ["col", "filter"];
+		return ["col", "filter", "label"];
 	}
 
 	get col(): string {
-		return this.getAttribute("col")?.trim().toLowerCase() || "";
+		return this.getAttribute("col") || "";
 	}
 
 	get filter(): string {
 		return this.getAttribute("filter")?.trim().toLowerCase() || "";
+	}
+
+	get label(): string {
+		return this.getAttribute("label") || this.col;
 	}
 
 	public connectedCallback(): void {
@@ -24,69 +25,17 @@ export class ActionTableFilterSwitch extends HTMLElement {
 	}
 
 	private render(): void {
-		const css = `<style>
-        :host {
-            --action-table-filter-switch-focus dodgerblue;
-            --action-table-filter-switch-unchecked: lightgray;
-            --action-table-filter-switch-checked: green;
-        }
-        label {
-            display: inline-flex;
-            align-items: center;
-        }
-        input {
-            appearance: none;
-            position: relative;
-            display: inline-block;
-            background: var(--action-table-filter-switch-unchecked);
-            cursor: pointer;
-            height: 1.4em;
-            width: 2.75em;
-            vertical-align: middle;
-            border-radius: 2em;
-            box-shadow: 0px 1px 3px #0003 inset;
-            transition: 0.25s linear background;
-            margin-inline-end: 0.5em;
-          }
-          input::before {
-            content: "";
-            display: block;
-            width: 1em;
-            height: 1em;
-            background: #fff;
-            border-radius: 1em;
-            position: absolute;
-            top: 0.2em;
-            left: 0.2em;
-            box-shadow: 0px 1px 3px #0003;
-            transition: 0.25s linear transform;
-            transform: translateX(0rem);
-          }
-          :checked {
-            background: var(--action-table-filter-switch-checked);
-          }
-          :checked::before {
-            transform: translateX(1rem);
-          }
-          input:focus {
-            outline: transparent;
-          }
-          input:focus-visible {
-            outline: 2px solid var(--action-table-filter-switch-focus);
-            outline-offset: 2px;
-          }
-          </style>`;
-		const html = `<label>
+		const html = `<label class="switch">
         <input type="checkbox" />
-        <slot>Show Only ${this.filter} for ${this.col}</slot>
+        <span>${this.label}</span>
       </label>`;
 
-		this.shadow.innerHTML = `${css}${html}`;
+		this.innerHTML = `${html}`;
 	}
 
 	private addEventListeners(): void {
 		// Add event listener that detects changes in the select element
-		this.shadow.addEventListener("click", (event) => {
+		this.addEventListener("click", (event) => {
 			const target = event.target as HTMLInputElement;
 			let value = target.checked ? this.filter : "";
 			let detail = { col: this.col, value };
@@ -95,7 +44,7 @@ export class ActionTableFilterSwitch extends HTMLElement {
 	}
 
 	public resetFilter() {
-		const input = this.shadow.querySelector("input");
+		const input = this.querySelector("input");
 		if (input) {
 			input.checked = false;
 		}
